3427654eded8787fcd57bf6b4151c11ac8072b7c
nikoladubica <45574682+nikoladubica@users.noreply.github.com>
2026-06-11 15:24:42 +0200

    Fix white buttons

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-16ad23c7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.efpir7vukrs"
+    "revision": "0.o4hh1r2jgo8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
